4e09bf4a85d64cf290d50d59820e3f50061ad413
Gabriel Georgiev <44534520+GabrielGeorgievGit@users.noreply.github.com>
2021-07-29 15:34:53 +0300

    Create Solution.java

---
+++ WhoLikesIt/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static String whoLikesIt(String... names) {
+      int size = names.length;
+      switch(size)
+      {
+      case 0: return "no one likes this";
+      case 1: return names[0] + " likes this";
+      case 2 : return names[0] + " and " + names[1] + " like this";
+      case 3: return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+      default:
+        return names[0] + ", " + names[1] + " and " + Integer.toString(size - 2) + " others like this";
+      }
+    }
+}
